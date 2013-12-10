905464dd00b3a934c9d9d1989f090e19fe9d7a34
Jake Naviasky <jnaviask@andrew.cmu.edu>
2013-12-10 18:21:29 -0500

    Simplified heavily, better than ever

--- slicer.py
+++ slicer.py
@@ -7,6 +7,8 @@ import argparse
 import numpy as np
 import scipy.io.wavfile as sp
 
+verbose = True
+
 def samples_to_string(samples, rate, usems=False):
     minutes = (samples / rate) / 60
     seconds = (samples / rate) % 60
@@ -38,76 +40,43 @@ def get_amplitudes(d, rate, samples_per_slice, shift_per_slice):
                            'mean': np.mean(sample)})
     return amplitudes
 
-def get_deltas(amps):
-    deltas = []
-    for i in xrange(len(amps) - 1):
-        deltas.append({
-            'delta': amps[i+1]['mean'] - amps[i]['mean'],
-            'start_val': amps[i]['mean'],
-            'end_val': amps[i+1]['mean'],
-            'start_sample': amps[i]['start'],
-            'end_sample': amps[i+1]['end']
-        })
-    return deltas
-
-def find_track_gaps(d, rate, num_gaps=None):
-    min_song_len = rate * 60
+def find_gaps(amps, num=None, threshold=800, max_song_len=(44100*60)):
+    all_gaps = filter(lambda x: x['mean'] < threshold, amps)
     gaps = []
-    small_delta_scale = 2
-    large_delta_scale = 1. / 2
-    small_amp_scale = 800
-    # This messy piece of crap filter the thing.
-    time_per_gap = d[0]['end_sample'] - d[0]['start_sample']
-    gaps_to_remove = min_song_len / time_per_gap
-    dr = d[gaps_to_remove:len(d) - gaps_to_remove]
-    dr_low_amp = [dr[i] for i in xrange(len(dr) - 1) if (dr[i]['start_val'] + dr[i]['end_val'] / 2) < small_amp_scale or (dr[i-1]['start_val'] + dr[i-1]['end_val']) / 2 < small_amp_scale]
-    max_delta = max(dr_low_amp, key=lambda x: x['delta'])['delta']
-    min_delta = abs(min(dr_low_amp, key=lambda x: abs(x['delta']))['delta'])
-    #print max_delta, min_delta
-    for i in xrange(len(dr_low_amp) - 1):
-        gap = dr_low_amp[i]
-        chg = dr_low_amp[i+1]
-        # goal: gap has small delta, chg has large delta
-        #     gap has small amplitude, chg has large amplitude
-        score = abs(gap['delta']) / min_delta + max_delta / chg['delta']
-        gaps.append({'gap': gap, 'chg': chg, 'score': score})
-    results = []
-    if (num_gaps is None):
-        # make arbitrary bounds
-        large_delta = 300
-        small_delta = 300
-        small_amp = 800
-        for i in gaps:
-            if (abs(i['gap']['delta']) < small_delta and
-                    i['chg']['delta'] > large_delta and
-                    (gap['start_val'] + gap['end_val'] / 2) < small_amp and
-                    (len(results) == 0 or
-                        (results[-1]['end_sample'] + min_song_len
-                            < i['gap']['start_sample']))):
-                results.append(i['gap'])
-        return results
+    for i in xrange(len(all_gaps)):
+        gap = all_gaps[i]
+        if (len(gaps) == 0 or gaps[-1]['end'] + 44100 * 60 < gap['end']):
+            gaps.append(gap)
+    if verbose: pprint.pprint(gaps)
+    if num is None:
+        return gaps
     else:
-        sorted_gaps = sorted(gaps, key=lambda x: x['score'], reverse=True)
-        idx = 0
-        while (len(results) < num_gaps - 1):
-            item = sorted_gaps[idx]['gap']
-            far_enough = [(abs(item['end_sample'] - x['end_sample'])
-                           > min_song_len) for x in results]
-            if (all(far_enough)):
-                #print "Found gap at idx", idx
-                results.append(item)
-            idx += 1
-        return sorted(results, key=lambda x: x['start_sample'])
-    
+        return sorted(sorted(gaps, key=lambda x: x['mean'])[0:num],
+                      key=lambda x: x['start'])
+
+def gap_shift(data, rate, gaps, shift=0.1, scale=1.5, pullback=0.5):
+    results = []
+    maxgap = max(gaps, key=lambda x: x['mean'])['mean'] * scale
+    for gap in gaps:
+        start = gap['end'] + rate * shift
+        end = 0
+        while end == 0 or next_vol < maxgap * scale:
+            end = start + rate * shift
+            next_vol = np.mean(abs(np.mean(data[start:end], 1)))
+            start += rate * shift
+        results.append(int(end - rate * (shift + pullback)))
+    return results
+
 def process_audio_file(data, rate, num_gaps=None):
-    amps = get_amplitudes(data, rate, 1, 0.5)
-    deltas = get_deltas(amps)
-    track_pts = find_track_gaps(deltas, rate, num_gaps=num_gaps)
+    max_song_len = rate * 60
+    amps = get_amplitudes(data, rate, 2, 1)
+    gaps = find_gaps(amps, num=num_gaps, max_song_len=max_song_len)
+    if (gaps[-1]['end'] + max_song_len > len(data)):
+        gaps = gaps[0:len(gaps) - 1]
+    track_pts = gap_shift(data, rate, gaps)
     print "Found %d songs." % (len(track_pts) + 1)
-    #pprint.pprint(track_pts)
-    pullback = rate / 2
-    song_starts = [i['end_sample'] - pullback for i in track_pts]
-    return song_starts
+    if verbose: pprint.pprint(track_pts)
+    return track_pts
 
 def chop_audio_file(data, rate, song_starts, save_dir):
     # Last item in song_starts should be the end of the last file
@@ -148,16 +117,18 @@ if __name__ == "__main__":
     parser.add_argument("outfile",
                         help="output filename (for -d) or directory")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-d", "--detect",
-                       help="detect track starts and write to file",
-                       action="store_true")
+    group.add_argument("-d", "--detect", action="store_true",
+                       help="detect track starts and write to file")
     group.add_argument("-c", "--chop", type=str,
                        metavar="slice-file",
                        help="chop file using given track start file")
     parser.add_argument("-t", "--tracks", type=int, metavar="num-tracks",
                         help="number of tracks in input file")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="toggle more verbose output")
     args = parser.parse_args()
 
+    verbose = args.verbose
     (rate, data) = sp.read(args.infile, mmap=True)
     if args.detect:
         starts = process_audio_file(data, rate, num_gaps=args.tracks)
